e145f745a5deff5942fc60cd4b1ab56cf4a49d41
Dein Name <deine.email@example.com>
2025-01-04 15:23:31 +0100

    New Features

--- src/components/pricing/page.tsx
+++ src/components/pricing/page.tsx
@@ -20,7 +20,7 @@ export default function PricingPage() {
     };
 
     const Standardprice = isToggleOn ? "€7.99" : "€4.99";
-    const Premiumprice = isToggleOn ? "€7.99" : "€4.99";
+    const Premiumprice = isToggleOn ? "€11.99" : "€7.99";
 
     return (
         <>
@@ -53,22 +53,39 @@ export default function PricingPage() {
                         alignItems: "center",
                         width: "100%",
                         height: "100%",
-                        gap: "30px",
-                        marginTop: "50px",
+                        gap: "10%",
+                        marginTop: "33px",
                         flexWrap: "wrap",
                     }}
                 >
                     {/* Standard Plan */}
                     <div
-                        id='ContainerStandard'
+                        id="ContainerStandard"
                         style={{
-                            height: "640px",
-                            width: "280px",
+                            height: "660px",
+                            maxWidth: "300px",
+                            width: "100%",
                             border: "1px solid #f2c76e",
                             borderRadius: "8px",
                             marginBottom: "20px",
+                            position: "relative",
+                            overflow: "hidden",
                         }}
                     >
+                        {/* Hälfte mit rotem Hintergrund */}
+                        <div
+                            style={{
+                                backgroundColor: "rgba(255,255,255,0.1)",
+                                height: "50%",
+                                width: "100%",
+                                position: "absolute",
+                                bottom: "0",
+                                left: "0",
+                                
+                            }}
+                        ></div>
+
+                        {/* Inhalt */}
                         <div
                             style={{
                                 width: "100%",
@@ -76,37 +93,100 @@ export default function PricingPage() {
                                 display: "flex",
                                 alignItems: "center",
                                 justifyContent: "center",
+                                position: "relative",
+                                 // Inhalt über den Hintergrund legen
                             }}
                         >
                             <WorkIcon style={{ scale: "2.5" }} />
                         </div>
-                        <div style={{ width: "100%", textAlign: "center" }}>
+                        <div style={{ width: "100%", textAlign: "center", position: "relative",  }}>
                             <h1 style={{ margin: "0" }}>Standard</h1>
                         </div>
-                        <div style={{ textAlign: "center" }}>
+                        <div style={{ textAlign: "center", position: "relative",  }}>
                             <h4 style={{ fontSize: "40px", margin: "20px" }}>{Standardprice}</h4>
                         </div>
 
                         {ContentArrayStandard.map((ContentStandard) => (
-                            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }} key={ContentStandard.Content}><p style={{ width: "100%", textAlign: "center", position: "relative", top: "-15px", margin: "6px" }}><CheckCircleOutlineIcon style={{ position: "relative", top: "5px", scale: "0.8" }} />{ContentStandard.Content}</p></div>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "column",
+                                    position: "relative",
+                                    
+                                }}
+                                key={ContentStandard.Content}
+                            >
+                                <p
+                                    style={{
+                                        width: "100%",
+                                        textAlign: "center",
+                                        position: "relative",
+                                        top: "-15px",
+                                        margin: "6px",
+                                    }}
+                                >
+                                    <CheckCircleOutlineIcon style={{ position: "relative", top: "5px", scale: "0.8" }} />
+                                    {ContentStandard.Content}
+                                </p>
+                            </div>
                         ))}
 
-                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "5px" }}>
-                            <button style={{ cursor: "pointer", fontSize: "18px", borderRadius: "30px", border: "2px solid #f2c76e", color: "#f2c76e", padding: "10px 30px", backgroundColor: "inherit" }}>Buchen</button>
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginTop: "10px",
+                                position: "relative",
+                                
+                            }}
+                        >
+                            <button
+                                style={{
+                                    cursor: "pointer",
+                                    fontSize: "18px",
+                                    borderRadius: "30px",
+                                    border: "2px solid #f2c76e",
+                                    color: "#f2c76e",
+                                    padding: "10px 30px",
+                                    backgroundColor: "inherit",
+                                }}
+                            >
+                                Buchen
+                            </button>
                         </div>
                     </div>
 
                     {/* Premium Plan */}
                     <div
-                        id='ContainerPremium'
+                        id="ContainerStandard"
                         style={{
-                            height: "640px",
-                            width: "280px",
+                            height: "660px",
+                            maxWidth: "300px",
+                            width: "100%",
                             border: "1px solid #f2c76e",
                             borderRadius: "8px",
                             marginBottom: "20px",
+                            position: "relative",
+                            overflow: "hidden",
                         }}
                     >
+                        {/* Hälfte mit rotem Hintergrund */}
+                        <div
+                            style={{
+                                backgroundColor: "rgba(255,255,255,0.1)",
+                                height: "50%",
+                                width: "100%",
+                                position: "absolute",
+                                bottom: "0",
+                                left: "0",
+                                
+                            }}
+                        ></div>
+
+                        {/* Inhalt */}
                         <div
                             style={{
                                 width: "100%",
@@ -114,23 +194,69 @@ export default function PricingPage() {
                                 display: "flex",
                                 alignItems: "center",
                                 justifyContent: "center",
+                                position: "relative",
+                                 // Inhalt über den Hintergrund legen
                             }}
                         >
                             <TbPremiumRights style={{ scale: "4" }} />
                         </div>
-                        <div style={{ width: "100%", textAlign: "center" }}>
+                        <div style={{ width: "100%", textAlign: "center", position: "relative",  }}>
                             <h1 style={{ margin: "0" }}>Premium</h1>
                         </div>
-                        <div style={{ textAlign: "center" }}>
+                        <div style={{ textAlign: "center", position: "relative",  }}>
                             <h4 style={{ fontSize: "40px", margin: "20px" }}>{Premiumprice}</h4>
                         </div>
 
                         {ContentArrayPremium.map((ContentPremium) => (
-                            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }} key={ContentPremium.Content}><p style={{ width: "100%", textAlign: "center", position: "relative", top: "-15px", margin: "6px" }}><CheckCircleOutlineIcon style={{ position: "relative", top: "5px", scale: "0.8" }} />{ContentPremium.Content}</p></div>
-
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "center",
+                                    flexDirection: "column",
+                                    position: "relative",
+                                    
+                                }}
+                                key={ContentPremium.Content}
+                            >
+                                <p
+                                    style={{
+                                        width: "100%",
+                                        textAlign: "center",
+                                        position: "relative",
+                                        top: "-15px",
+                                        margin: "6px",
+                                    }}
+                                >
+                                    <CheckCircleOutlineIcon style={{ position: "relative", top: "5px", scale: "0.8" }} />
+                                    {ContentPremium.Content}
+                                </p>
+                            </div>
                         ))}
-                        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "0px", }}>
-                            <button style={{ cursor: "pointer", fontSize: "18px", borderRadius: "30px", border: "2px solid #f2c76e", color: "#f2c76e", padding: "10px 30px", backgroundColor: "inherit" }}>Buchen</button>
+
+                        <div
+                            style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginTop: "10px",
+                                position: "relative",
+                                
+                            }}
+                        >
+                            <button
+                                style={{
+                                    cursor: "pointer",
+                                    fontSize: "18px",
+                                    borderRadius: "30px",
+                                    border: "2px solid #f2c76e",
+                                    color: "#f2c76e",
+                                    padding: "10px 30px",
+                                    backgroundColor: "inherit",
+                                }}
+                            >
+                                Buchen
+                            </button>
                         </div>
                     </div>
                 </div>
