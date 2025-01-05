36fc523300677d0f34d914c07e0900d7a6111d76
Dein Name <deine.email@example.com>
2025-01-05 15:40:34 +0100

    New Features

--- src/components/kontakt/page.tsx
+++ src/components/kontakt/page.tsx
@@ -54,180 +54,198 @@ export default function KontaktPage() {
           <h2>Kontakt</h2>
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column",
+              border: "1px solid #f2c76e",
+              padding: "25px 20px",
+              borderRadius: "8px",
+              width: "83%",
+              maxWidth: "900px",
             }}
           >
             <div
               style={{
-                border: "1px solid #f2c76e",
-                padding: "25px 20px",
-                borderRadius: "8px",
-                width: "900px",
+                marginLeft: "40px",
+                lineHeight: "20px",
+                display: "flex",
+                justifyContent: "left",
+                alignItems: "flex-start",
+                flexDirection: "column",
               }}
             >
-              <div
+              <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
+                Adresse:
+              </h3>
+              <p>Beispielstraße</p>
+              <p>Postleitzahl, Deutschland</p>
+
+              <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
+                Email:
+              </h3>
+              <p>info@deinefirma.de</p>
+
+              <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
+                Telefon:
+              </h3>
+              <p>Du erreichst uns unter:</p>
+              <p>+49 123 456 789</p>
+            </div>
+          </div>
+
+
+
+          <form onSubmit={handleSubmit} style={{
+            width: "90%",
+            maxWidth: "940px",
+            margin: "0",
+            padding: "0",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+
+
+          }}>
+            <div
+              style={{
+                marginBottom: "40px",
+                width: "100%",
+                display: "flex",
+                justifyContent: "space-evenly",
+                marginTop: "40px",
+                gap: "20px"
+              }}
+            >
+              <input
+                type="text"
+                name="name"
                 style={{
-                  marginLeft: "40px",
-                  lineHeight: "20px",
-                  display: "flex",
-                  justifyContent: "left",
-                  alignItems: "flex-start",
-                  flexDirection: "column",
+                  border: "1px solid #f2c76e",
+                  borderRadius: "8px",
+                  width: "50%",
+                  fontSize: "20px",
+                  backgroundColor: "inherit",
+                  padding: "20px",
+                  color: "#fff",
                 }}
-              >
-                <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
-                  Adresse:
-                </h3>
-                <p>Beispielstraße</p>
-                <p>Postleitzahl, Deutschland</p>
+                placeholder="Name*"
+                required
+                value={formData.name}
+                onChange={handleChange}
+              />
 
-                <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
-                  Email:
-                </h3>
-                <p>info@deinefirma.de</p>
-
-                <h3 style={{ fontSize: "30px", marginBottom: "10px" }}>
-                  Telefon:
-                </h3>
-                <p>Du erreichst uns unter:</p>
-                <p>+49 123 456 789</p>
-              </div>
+              <input
+                type="email"
+                name="email"
+                style={{
+                  border: "1px solid #f2c76e",
+                  borderRadius: "8px",
+                  padding: "20px",
+                  fontSize: "20px",
+                  width: "50%",
+                  backgroundColor: "inherit",
+                  color: "#fff",
+                }}
+                placeholder="Email*"
+                required
+                value={formData.email}
+                onChange={handleChange}
+              />
             </div>
 
-            <form onSubmit={handleSubmit}>
-              <div
+            <div
+              style={{
+                display: "flex",
+                width: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+                marginBottom: "40px"
+              }}
+            >
+              <input
+                type="text"
+                name="subject"
                 style={{
-                  marginBottom: "40px",
-                  display: "flex",
+                  border: "1px solid #f2c76e",
+                  borderRadius: "8px",
+                  padding: "20px",
+                  fontSize: "20px",
                   width: "100%",
-                  justifyContent: "space-evenly",
-                  gap: "20px",
-                  marginTop: "40px",
+                  backgroundColor: "inherit",
+                  color: "#fff",
                 }}
-              >
-                <input
-                  type="text"
-                  name="name"
-                  style={{
-                    border: "1px solid #f2c76e",
-                    borderRadius: "8px",
-                    width: "50%",
-                    fontSize: "20px",
-                    backgroundColor: "inherit",
-                    padding: "20px",
-                    color: "#fff",
-                  }}
-                  placeholder="Name*"
-                  required
-                  value={formData.name}
-                  onChange={handleChange}
-                />
+                required
+                placeholder="Betreff*"
+                value={formData.subject}
+                onChange={handleChange}
+              />
 
-                <input
-                  type="email"
-                  name="email"
-                  style={{
-                    border: "1px solid #f2c76e",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    fontSize: "20px",
-                    width: "50%",
-                    backgroundColor: "inherit",
-                    color: "#fff",
-                  }}
-                  placeholder="Email*"
-                  required
-                  value={formData.email}
-                  onChange={handleChange}
-                />
-              </div>
+            </div>
 
-              <div
+
+            <div style={{
+              display: "flex",
+              width: "100%",
+              marginBottom: "40px"
+
+            }}>
+              <textarea
+                name="message"
                 style={{
-                  display: "flex",
+                  backgroundColor: "inherit",
+                  height: "200px",
+                  border: "1px solid #f2c76e",
+                  borderRadius: "8px",
+                  padding: "20px",
                   width: "100%",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: "40px",
+                  fontSize: "20px",
+                  color: "#fff",
                 }}
+                placeholder="Schreibe eine Nachricht*"
+                required
+                value={formData.message}
+                onChange={handleChange}
+              ></textarea>
+
+            </div>
+
+
+
+            {/* ------------------------- */}
+
+            <div
+              style={{
+                marginBottom: "50px",
+                width: "100%",
+                display: "flex",
+                alignItems: "flex-start",
+              }}
+            >
+              <button
+                style={{
+                  backgroundColor: "inherit",
+                  color: "#f2c76e",
+                  border: "1px solid #f2c76e",
+                  borderRadius: "50px",
+                  padding: "20px 80px",
+                  fontSize: "20px",
+                  cursor: "pointer",
+                }}
+                type="submit"
               >
-                <input
-                  type="text"
-                  name="subject"
-                  style={{
-                    border: "1px solid #f2c76e",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    fontSize: "20px",
-                    width: "900px",
-                    backgroundColor: "inherit",
-                    color: "#fff",
-                  }}
-                  required
-                  placeholder="Betreff*"
-                  value={formData.subject}
-                  onChange={handleChange}
-                />
+                Abschicken
+              </button>
+            </div>
+          </form>
 
-                <textarea
-                  name="message"
-                  style={{
-                    backgroundColor: "inherit",
-                    height: "200px",
-                    border: "1px solid #f2c76e",
-                    borderRadius: "8px",
-                    padding: "20px",
-                    width: "900px",
-                    fontSize: "20px",
-                    color: "#fff",
-                  }}
-                  placeholder="Schreibe eine Nachricht*"
-                  required
-                  value={formData.message}
-                  onChange={handleChange}
-                ></textarea>
+          {error &&
+            <div style={{ position: "relative", top: "-100px", left: "20px" }}>
+              <p style={{ color: "red" }}>{error}</p>
+            </div>}
 
-                <div
-                  style={{
-                    marginBottom: "50px",
-                    width: "100%",
-                    display: "flex",
-                    alignItems: "flex-start",
-                  }}
-                >
-                  <button
-                    style={{
-                      backgroundColor: "inherit",
-                      color: "#f2c76e",
-                      border: "1px solid #f2c76e",
-                      borderRadius: "50px",
-                      padding: "20px 80px",
-                      fontSize: "20px",
-                      cursor: "pointer",
-                    }}
-                    type="submit"
-                  >
-                    Abschicken
-                  </button>
-                </div>
-              </div>
-            </form>
-
-            {error &&
-              <div style={{ position: "relative", top: "-100px", left: "20px" }}>
-                <p style={{ color: "red" }}>{error}</p>
-              </div>}
-
-            {success &&
-              <div style={{ position: "relative", top: "-100px", left: "20px" }}>
-                <p style={{ color: "green" }}>{success}</p>
-              </div>
-            }
-          </div>
+          {success &&
+            <div style={{ position: "relative", top: "-100px", left: "20px" }}>
+              <p style={{ color: "green" }}>{success}</p>
+            </div>
+          }
         </div>
       </div>
     </>
