925f5d2c5b2083846514edc2b45b8830d29cf86a
Dein Name <deine.email@example.com>
2025-01-09 12:19:47 +0100

    New Features

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     return (
         <>
             {/* <div className='HomeImg '></div> */}
+
             <div className='ContainerHome' >
 
                 <div className='HeaderContent'>
@@ -16,18 +17,16 @@ export default function Home() {
                     </h1>
 
                     <p className='HeaderSubText'>
-                    Mit Linkify übertragen Sie Daten mühelos zwischen Buchungssoftware und DMS. Sparen Sie Zeit, reduzieren Sie manuelle Arbeit und optimieren Sie Ihre Abläufe.
+                        Mit Linkify übertragen Sie Daten mühelos zwischen Buchungssoftware und DMS. Sparen Sie Zeit, reduzieren Sie manuelle Arbeit und optimieren Sie Ihre Abläufe.
                     </p>
                 </div>
-                <div className='BtnHomeContainer'>
 
+                <div className='BtnHomeContainer'>
                     <div className='BtnHome'>
                         <Link href={"#PriceHeadline"} className='BtnDesign'>Get Started</Link >
                         <Link href={"/product"} className='BtnDesign'>Produktseite</Link >
                     </div>
                 </div>
-
-
 
                 <div id="particles-js">
                     <Script src="/js/HeaderAnimationCanva/particles.js" />
