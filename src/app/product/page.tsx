8241f87b16f7c22859b351cd645a5109495c9f75
Dein Name <deine.email@example.com>
2025-01-13 20:50:16 +0100

    New Features

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,51 +6,53 @@ import React from 'react';
 export default function ProductPage() {
 
   const TargetElementItems = [
-    { Element: "Gesetzliche Vorgaben: Firmen mit mindestens 60.000 € Gewinn oder mindestens 300.000 € Umsatz müssen Unterlagen revisionssicher und DSGVO-konform speichern." },
-    { Element: "Strafen: 2 % des Jahresumsatzes, max. 2,5 Mio. € für Alle Unternehmen in der Gruppe." },
-    { Element: "Aufbewahrung: Bis zu 10 Jahre je nach Dokumentart." },
-    { Element: "DMS-Lösung: Erfüllt gesetzliche Anforderungen." },
-    { Element: "Ab dem 1. Januar  2025: Firmen müssen Rechnungen digital erstellen, empfangen und im DMS speichern." },
-    { Element: "Aktueller Prozess: Manuell, zeit- und kostenintensiv." },
+    { Element: "Gesetzliche Vorgaben: Firmen mit mindestens 60.000 € Gewinn oder mindestens 300.000 € Umsatz müssen Unterlagen revisionssicher und DSGVO-konform speichern" },
+    { Element: "Strafen: 2 % des Jahresumsatzes, max. 2,5 Mio. € für Alle Unternehmen in der Gruppe" },
+    { Element: "Aufbewahrung: Bis zu 10 Jahre je nach Dokumentart" },
+    { Element: "DMS-Lösung: Erfüllt gesetzliche Anforderungen" },
+    { Element: "Ab dem 1. Januar  2025: Firmen müssen Rechnungen digital erstellen, empfangen und im DMS speichern" },
+    { Element: "Aktueller Prozess: Manuell, zeit- und kostenintensiv" },
   ];
 
   const TargetElementItems1 = [
-    { Element: "Linkify vereinfacht die Datenübertragung." },
-    { Element: "Reduziert manuellen, fehleranfälligen Aufwand." },
-    { Element: "Verbindet Buchhaltungssoftware mit Dokumentensoftware." },
-    { Element: "Ermöglicht effizientere Arbeitsweise." },
-    { Element: "Sorgt für eine reibungslose Datenübertragung." },
-    { Element: "Alles Regelkonform." },
+    { Element: "Linkify vereinfacht die Datenübertragung" },
+    { Element: "Reduziert manuellen, fehleranfälligen Aufwand" },
+    { Element: "Verbindet Buchhaltungssoftware mit Dokumentensoftware" },
+    { Element: "Ermöglicht effizientere Arbeitsweise" },
+    { Element: "Sorgt für eine reibungslose Datenübertragung" },
+    { Element: "Alles Regelkonform" },
   ];
 
   const TargetElementItems2 = [
-    { Element: "Optimierung des Datenaustauschs durch weniger Manuelles Handeln." },
-    { Element: "Minimierung menschlicher Fehler." },
-    { Element: "Reduzierung der Bearbeitungs- und Arbeitszeit." },
+    { Element: "Datenaustausch durch Automatisierung" },
+    { Element: "Minimierung menschlicher Fehler" },
+    { Element: "5 Sprachen (Internationalisierung)" },
+    { Element: "Reduzierung der Bearbeitungs- und Arbeitszeit" },
     { Element: "Steigerung der Gesamteffizienz in Geschäftsprozessen" },
   ];
 
   const TargetElementItems3 = [
-    { Element: "Benutzerfreundliche Oberfläche von Linkify." },
-    { Element: "Einfache Einrichtung ohne technische Vorkenntnisse." },
-    { Element: "Nahtlose Integration in den Arbeitsalltag." },
-    { Element: "Keine aufwendigen Anpassungen an bestehende Systeme nötig." },
-    { Element: "Zeit- und Kostenersparnis durch einfache Konfiguration." }
+    { Element: "Benutzerfreundliche Oberfläche von Linkify" },
+    { Element: "Einfache Einrichtung ohne technische Vorkenntnisse" },
+    { Element: "Nahtlose Integration in den Arbeitsalltag" },
+    { Element: "Keine aufwendigen Anpassungen an bestehende Systeme nötig" },
+    { Element: "Zeit- und Kostenersparnis durch einfache Konfiguration" }
   ];
 
 
   const TargetElementItems4 = [
-    { Element: "Linkify gewährleistet Transparenz in den übertragenen Daten." },
-    { Element: "Alle Informationen werden korrekt und vollständig übertragen." },
-    { Element: "Kontrolle ermöglicht effizientere und sicherere Buchhaltungsprozesse." }
+    { Element: "Linkify gewährleistet Transparenz in den übertragenen Daten" },
+    { Element: "Alle Informationen werden korrekt und vollständig übertragen" },
+    { Element: "Es werden Keine Daten gespeichert, sondern Live von A nach B transportiert" },
   ];
-
-
+  
+  
   const TargetElementItems5 = [
-    { Element: "Sensible Informationen durch Verschlüsselung geschützt(HTTPS-Protokoll)." },
-    { Element: "Datenübertragung mit HTTPS-Protokollen für Datensicherheit." },
-    { Element: "Es werden Keine Daten gespeichert, sondern Live von A nach B transportiert." }
+    { Element: "Sensible Informationen durch Verschlüsselung geschützt(HTTPS-Protokoll)" },
+    { Element: "Daten vor unbefugtem Zugriff geschützt" },
+    { Element: "Strikte Datentrennung, Rollen: Superadmin, Admin, User, Mandantenfähigkeit" },
   ];
+
 
   const TargetItem = {
     Titel: "Das Problem",
